2c630660e91f092ff5922869cdfbebcdd74e87d8
Jordan Nakamura <jcnrad@msn.com>
2022-09-08 22:04:19 -0700

    Commit test
    Add camera movement
    (testing commit to branch)

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    public GameObject foo;
+
     private void Awake()
     {
         Debug.Log("Gotteem!!");
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            foo.transform.Translate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            foo.transform.Translate(Vector3.back);
+        }
     }
 }
