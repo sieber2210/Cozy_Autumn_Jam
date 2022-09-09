5d06518f0e7b53e3800944ed283b29f9c75be006
sieber2210 <jsieber53@gmail.com>
2022-09-08 21:42:38 -0700

    Test update
    Gotteem!

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Gotteem!!");
     }
 }
